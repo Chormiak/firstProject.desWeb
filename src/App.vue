d3078893dcb318a8b7b682f38705e3c81d924575
Chormiak <william.chormi@gmail.com>
2025-03-18 09:18:36 -0300

    completed the simple calculator

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
+  import { reactive, computed } from 'vue';
 
+  const numbers = reactive({ true: 0, false: 0 });
+
+  function calculate(boolean, local) {
+    return numbers[boolean] += (local) ? 1 : -1;
+  }
+  const sum = computed(() => numbers.true + numbers.false);
 </script>
 
 <template>
-  <h1>Hello Word!</h1>
+  <h1>Calculator</h1>
+  
+  <div v-for="boolean of [true, false]">
+    <p>Valor: {{ numbers[boolean] }}</p>
+    <button @click="calculate(boolean, true)">Incrementar</button>
+    <button @click="calculate(boolean, false)">Decrementar</button>
+  </div>
+  <p>Soma: {{ sum }}</p>
 </template>
 
-<style scoped>
+<style scoped> 
 
 </style>
